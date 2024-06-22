07775c77ace4710004dcc1f747a0c7f7dcdfc4da
Som1326 <sebastianorregomarin@gmail.com>
2024-06-22 13:15:37 -0500

    Fixed

--- src/server.js
+++ src/server.js
@@ -12,53 +12,40 @@ const io = new Server({
     }
 })
 
-let players = [];
+/**
+ * Start listening on the specified port.
+ */
+io.listen(port);
 
+/**
+ * Listen for incoming connections.
+ */
 io.on("connection", (socket) => {
+  /**
+   * Log the ID of the player connected.
+   */
+  console.log(
+    "Player joined with ID",
+    socket.id,
+    ". There are " + io.engine.clientsCount + " players connected."
+  );
+
+  /**
+   * Handle a player's movement.
+   * Broadcast the transforms to other player.
+   */
+  socket.on("player-moving", (transforms) => {
+    socket.broadcast.emit("player-moving", transforms);
+  });
+
+  /**
+   * Handle player disconnection.
+   */
+  socket.on("disconnect", () => {
     console.log(
-        'Player joined with ID' +
-        socket.id +
-        '. There are ' +
-        io.engine.clientsCount +
-        ' players connected'
-    )
-
-    socket.on('players-connected', ()=>{
-        const player = players.find((player) => player.id === socket.id);
-
-        const knigth = "/assets/models/characters/caracterKnigth.glb";
-        const robot = "/assets/models/characters/Robot.glb";
-
-        if (!player) {
-            players.push({
-                id: socket.id,
-                urlAvatar: io.engine.clientsCount === 1 ?
-                knigth :
-                robot,
-                position: null,
-                rotation: null,
-                animation: "Idle",
-            });
-            socket.emit("players-connected", players);
-        }
-    });
-
-    socket.on("player-moving", (transforms) => {
-        socket.broadcast.emit("player-moving", transforms);
-    });
-
-    socket.on("change-animation", (animation) => {
-        const player = players.find(player => player.id === socket.id)
-        player.animation = animation;
-        socket.broadcast.emit("update-animation", player);
-    })
-
-    socket.on('disconnect', () => {
-        console.log('Player disconnected with ID ' + socket.id)
-
-        players = players.filter(player => player.id !== socket.id)
-        socket.broadcast.emit('player-disconnected', socket.id)
-    });
-})
-
-io.listen(port)
+      "Player disconnected with ID",
+      socket.id,
+      ". There are " + io.engine.clientsCount + " players connected"
+    );
+  });
+});
